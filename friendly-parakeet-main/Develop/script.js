c0a58d54c5c769a7840e923e77601c6562d1059a
EduardoRam7 <edd_madrid@hotmail.com>
2021-08-29 00:57:47 -0500

    starting to change the way to get variables to the prompts

--- friendly-parakeet-main/Develop/script.js
+++ friendly-parakeet-main/Develop/script.js
@@ -1,26 +1,49 @@
 // Assignment code here
+// Global Variables
 var lowerCaseChars = "qwertyuiopasdfghjklzxcvbnm";
 var upperCaseChars = "QWERTYUIOPASDFGHJKLZXCVBNM";
 var numbersChars = "1234567890";
 var specialChars = "!#$%&()*+,-./:;<=>?@[]^_{|}~";
+var ask1, text;
+var ask2, text;
+var ask3, text;
+var ask4, text;
+
 function generatePassword (){
-var ask1 = prompt("Would you like your password to have lowercase characters?");
-if (ask1 === "yes" || "YES" || "Yes"){
-  writePassword + lowerCaseChars;
+ 
+  ask1 = document.getElementById(prompt("Would you like your password to have lowercase letters?").value);
+  if (ask1 === "yes" || "YES" || "Yes" || "Y"){
+    document.getElementById(lowerCaseChars,)
+    }
+    else if (ask1 === "no" || "NO" || "No" || "N") {
+    
+    }
+    
+  ask2 = document.getElementById(prompt("Would you like your password to have uppercase letters?").value);
+  if (ask2 === "yes" || "YES" || "Yes" || "Y"){
+    document.getElementById(upperCaseChars,)
+    }
+    else if (ask2 === "no" || "NO" || "No" || "N") {
+
+    }
+
+  ask3 = document.getElementById(prompt("Would you like your password to contain numbers?").value);
+  if (ask3 === "yes" || "YES" || "Yes" || "Y"){
+    document.getElementById(numbersChars,)
+    }
+    else if (ask3 === "no" || "NO" || "No" || "N") {
+
+    }
+
+  ask4 = document.getElementById(prompt("Would you like your password to contain special characters?").value);
+  if (ask4 === "yes" || "YES" || "Yes" || "Y"){
+    document.getElementById(specialChars,)
+    }
+    else if (ask4 === "no" || "NO" || "No" || "N") {
+
+    }
   }
-var ask2 = prompt("Would you like your password to have uppercase characters?");
-if (ask2 === "yes" || "YES" || "Yes"){
-  writePassword + upperCaseChars;
-  }
-var ask3 = prompt("Would you like your password to contain numbers?");
-if (ask3 === "yes" || "YES" || "Yes"){
-  writePassword + numbersChars;
-  }
-var ask4 = prompt("Would you like your password to contain special characters?");
-if (ask4 === "yes" || "YES" || "Yes"){
-  writePassword + specialChars;
-  }
-}
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
